aad0480afc09d22aa601ef200eeb6c915ae2df6a
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2019-01-15 14:33:23 +0300

    Fix right fingerprint and repo for the test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cloudlinux/imunify360-doc.git master:gh-pages
+git push -f git@github.com:cloudlinux/imunify360-doc-test.git master:gh-pages
 
 cd -

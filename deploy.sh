8f0e34cb1768e81443ff4f1b7f2e46003c2a0f5e
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2019-02-19 16:46:47 +0300

    Get back production settings

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yarn docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'docs-dev.imunify360.com' > CNAME
+echo 'docs.imunify360.com' > CNAME
 
 git config --global user.email "telepenin.nikolay@gmail.com"
 git config --global user.name "circle-ci"
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy [skip ci]'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cloudlinux/imunify360-doc-test.git master:gh-pages
+git push -f git@github.com:cloudlinux/imunify360-doc.git master:gh-pages
 
 cd -
